1a4e70f62f86dd87f4d02055da748f83a21884fd
Ramya Sree <ramyasree@Ramyas-Laptop.local>
2023-05-16 14:48:14 -0500

    mern -infrastruture

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+
+// POST /api/users
+router.post('/', usersCtrl.create);
+
+// POST /api/users/login
+router.post('/login', usersCtrl.login);
+
+// GET /api/users/check-token
+router.get('/check-token', usersCtrl.checkToken);
+
+module.exports = router;
---
+++ src/pages/NewOrderPage/NewOrderPage.js
@@ -0,0 +1,5 @@
+export default function NewOrderPage(){
+    return (
+        <div>NewOrderPage</div>
+    )
+}
